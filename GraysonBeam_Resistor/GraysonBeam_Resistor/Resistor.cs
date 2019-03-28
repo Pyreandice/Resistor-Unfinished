f731b7304d0f4ca9a2d13135d259d453731afeb6
Grayson Beam <33629589+Pyreandice@users.noreply.github.com>
2019-03-28 11:13:54 -0500

    Add files via upload

---
+++ GraysonBeam_Resistor/GraysonBeam_Resistor/Resistor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraysonBeam_Resistor
+{
+    public partial class Resistor : Form
+    {
+        public Resistor()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MakeList()
+        {
+            string currentLine = string.Empty;
+            System.IO.StreamReader ReadFile = new System.IO.StreamReader(@"BandInfo.txt");
+            while ((currentLine = File.ReadLine()) != null)
+            {
+                
+            }
+        }
+    }
+}
---
+++ GraysonBeam_Resistor/GraysonBeam_Resistor/ResistorClass.cs
@@ -0,0 +1,15 @@
+﻿namespace GraysonBeam_Resistor
+{
+    class ResistorClass
+    {
+        public double Color { get; set; }
+
+        public double SignificantDigit { get; set; }
+
+        public double Multiplier { get; set; }
+
+        public double Tolerance { get; set; }
+
+
+    }
+}
